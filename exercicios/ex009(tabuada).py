65e517600e6a71727fd703ba23c540755b04d97f
Gabuabuu <gabydavilaneves@gmail.com>
2024-10-07 21:08:55 -0300

    Ex: multiplication table system

--- exercicios/ex009(tabuada).py
+++ exercicios/ex009(tabuada).py
@@ -1,6 +1,5 @@
-multiplicador = int(input('Digite um numero'))
-#resultado = multiplicadores  * multiplicador
+multiplicador = int(input('Qual tabuada deseja ver? '))
 
 for i in range(1, 11):
   resultado = multiplicador * i
-  print(f'A multiplicação de {multiplicador} x {i} = {resultado}')
+  print(f'A multiplicação de {multiplicador} x {i:2} = {resultado}')
